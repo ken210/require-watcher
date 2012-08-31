a6d35af643caf6b8e9fef028c840aad88a5cddc2
ken210 <ken.rosaka@adbat-tesla.com.br>
2012-08-31 18:10:01 -0300

    Adding options to command-line (path/to/r.js), (path/to/builderFile), and help

--- watcher.js
+++ watcher.js
@@ -2,7 +2,8 @@ var fs = require('fs'),
 	sys = require('sys'),
 	exec = require('child_process').exec,
 	isJs = /\.js\b/,
-	filename = __filename.split('/');
+	filename = __filename.split('/'),
+	output = '\n',
 
 	filename = filename[filename.length - 1];
 
@@ -10,19 +11,33 @@ function puts(error, stdout, stderr) {
 	sys.puts(stdout);
 }
 
-function watchFile(file) {
+function watchFile(file, r, builder) {
+	if (r == null) {
+		r = 'r.js';
+	}
+	if (builder == null) {
+		builder = 'app.build.js';
+	}
 	fs.watchFile(file, function () {
 		console.log('\n----------------\n\033[0mModule \033[0;32m"' + file + '" \033[0mmodified\n----------------');
-		exec("node r.js -o app.build.js", puts);
+		exec("node " + r + " -o " + builder, puts);
 	});
 }
 
-fs.readdir('.', function (err, files) {
-	files.forEach(function (file) {
-		if (isJs.test(file) && file !== filename) {
-			watchFile(file);
-		}
+if (process.argv[2] === 'help') {
+	output += 'Usage: node watcher.js [path/to/r.js] [path/to/builderFile]\n';
+	output += '       node watcher.js';
+} else {
+	fs.readdir('.', function (err, files) {
+		files.forEach(function (file) {
+			if (isJs.test(file) && file !== filename) {
+				watchFile(file, process.argv[2], process.argv[3]);
+			}
+		});
 	});
-});
 
-console.log('\nWatcher.js:\nWatching \033[0;33m' + __dirname + '\033[0m');
+	output += 'watcher.js:\nwatching \033[0;33m' + __dirname + '\033[0m\n';
+	output += 'Type node watcher.js help for options';
+}
+
+console.log(output);
